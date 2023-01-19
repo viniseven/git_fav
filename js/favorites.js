a32e8b21e7214ee91f722ded2af336a066cedf6f
Marcus Vinicius <marcusv892@gmail.com>
2023-01-18 23:16:08 -0300

    Buscando dados da api, renderizando em tela, controles da aplicação

--- js/favorites.js
+++ js/favorites.js
@@ -1,27 +1,52 @@
+import { GithubUser } from "./GithubUser.js"
+
 export class Favorites {
     constructor(root){
         this.root = document.querySelector(root)
         this.load()
-
-        this.update()
     }
 
     load(){
-        this.entries = [
-            {
-                login: 'viniseven',
-                name: 'Marcus Vinícius',
-                public_repos: '76',
-                followers: '120000'
-            },
-            {
-                login: 'diego3g',
-                name: 'Diego Fernandes',
-                public_repos: '40',
-                followers: '180000'
-            }
-        ]
+        this.entries = JSON.parse(localStorage.getItem
+            ('@github-favorites:')) || []    
     }
+
+    save(){
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async adduser(username){
+        try{
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists){
+                throw new Error('Usuário já inserido')
+            }
+
+            const user = await GithubUser.search(username)
+           
+            if(user.login === undefined){
+                throw new Error('Usuário não encontrado')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        }catch(error){
+            alert(error.message)
+        }   
+    }
+    
+    delete(user){
+        const filteredEntries = this.entries
+        .filter(entry => entry.login !== user.login)
+    
+        this.entries = filteredEntries
+        this.update()
+        this.save()
+    }  
 }
 
 export class FavoritesView extends Favorites{
@@ -29,28 +54,51 @@ export class FavoritesView extends Favorites{
         super(root)
 
         this.tbody = this.root.querySelector('table tbody')
+        this.update()
+        this.onadd()
+    }
+
+    onadd(){
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () =>{
+            const { value } = this.root.querySelector('.search input')
+
+           this.adduser(value)
+        }
     }
 
     update(){
-        this.removeAllTr()
+       this.removeAllTr()
 
         this.entries.forEach(user => {
             const row = this.createRow()
 
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de perfil de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
             
-            this.tbody.append(row)
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Deseja exlcluir esse dado?')
+
+                if(isOk){
+                    this.delete(user)
+                }
+            }
+
+                this.tbody.append(row)    
         })
-
-        
-
     }
-
+    
     createRow(){
 
         const tr = document.createElement('tr')
 
-        const data = `
+        tr.innerHTML = `
             <td class="user">
                 <img src="https://github.com/viniseven.png" 
                 alt="Foto de perfil do Github">
@@ -70,13 +118,14 @@ export class FavoritesView extends Favorites{
                 <button class="remove">&times;</button>
             </td>    
         `
-        tr.innerHTML = data
+       
 
         return tr
     }
 
     removeAllTr(){
-        this.tbody.querySelectorAll('tr').forEach((tr) => {
+        const tbody = this.root.querySelector('table tbody')
+        tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         })
     }
